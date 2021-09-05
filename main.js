5e9ab774f9a9eeda201b769d58914e4e8f91f572
maanya1205 <77110811+maanya1205@users.noreply.github.com>
2021-09-05 20:38:03 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+img= "";
+function preload(){
+    img= loadImage("dog_cat.jpg");
+}
+function setup(){
+    canvas= createCanvas(640,420);
+    canvas.center();
+   
+}
+function draw(){
+    image(img,0,0,640,420);
+}
